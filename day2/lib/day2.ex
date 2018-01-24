c5292463ed87acfba4da59dcc6e21d3768b4b140
drpacman <paul.caporn@bbc.co.uk>
2018-01-24 09:18:54 +0000

    Add part 2 and re-factor to pass functions

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -3,42 +3,87 @@ defmodule Day2 do
   Documentation for Day2.
   """
 
-  def key_to_grid(key) do
-    x = rem(key - 1, 3)
-    y = div(key - 1, 3)
+  def key_to_grid_part1(key) do
+    x = rem(key - 1, 3) - 1
+    y = div(key - 1, 3) - 1
     [ x, y ]
   end
 
-  def grid_to_key([x,y]) do
-    ((y * 3) + x) + 1
+  def grid_to_key_part1([x,y]) do
+    ((y + 1) * 3) + x + 2
   end
   
-  def navigate(key, instr) do
+  def navigate_part1(key, instr) do
     case key do
       [ x, y ] ->
         case instr do
-          "U" -> [ x, max(y-1,0) ]
-          "D" -> [ x, min(y+1,2) ]
-          "L" -> [ max(x-1,0), y ]
-          "R" -> [ min(x+1,2), y ]
+          "U" -> [ x, max(y-1,-1) ]
+          "D" -> [ x, min(y+1,1) ]
+          "L" -> [ max(x-1,-1), y ]
+          "R" -> [ min(x+1,1), y ]
         end
        _ ->
-        navigate(key_to_grid(key), instr)
+        navigate_part1(key_to_grid_part1(key), instr)
     end
   end
 
-  def key_after_seq(key, instrs) do
-    pos = key_to_grid(key)
-    end_pos = Enum.reduce(String.codepoints(instrs), pos, &(navigate(&2,&1)))
-    grid_to_key(end_pos)
+  def key_to_grid_part2(key) do
+    cond do
+      key == 1 ->
+        [ 0, -2 ]
+      key >=2 && key <=4 ->
+        [ (key - 3), -1 ]
+      key >=5 && key <=9 ->
+        [ (key - 7), 0 ]
+      key >= 10 && key <= 12 ->
+        [ (key - 11), 1 ]
+      true ->
+        [ 0, 2 ]
+    end
   end
 
-  def generate_sequence(instrs) do 
-   Enum.reduce(instrs, [5], fn(instr, keys) -> ( [ key_after_seq(hd(keys), instr) ] ++ keys ) end) |> Enum.reverse |> tl
+  def grid_to_key_part2([x,y]) do
+    cond do
+      y == -2 ->
+        1
+      y == -1 ->
+        (x+3)
+      y == 0 ->
+        (x+7)
+      y == 1 ->
+        :binary.list_to_bin([?a + (x + 1)])
+      true ->
+        "d"
+    end
+  end
+  
+  def navigate_part2(key, instr) do
+    case key do
+      [ x, y ] ->
+        case instr do
+          "U" -> [ x, max(y-1, -(2-abs(x))) ]
+          "D" -> [ x, min(y+1, (2-abs(x))) ]
+          "L" -> [ max(x-1,-(2-abs(y))), y ]
+          "R" -> [ min(x+1, (2-abs(y))), y ]
+        end
+       _ ->
+        navigate_part2(key_to_grid_part2(key), instr)
+    end
   end
 
-  def generate_sequence_from_file( filename ) do
-    read_input(filename) |> generate_sequence
+  def key_after_seq(key, instrs, resolver) do
+    { navigator, key_to_grid, grid_to_key } = resolver
+    pos = key_to_grid.(key)
+    end_pos = Enum.reduce(String.codepoints(instrs), pos, &(navigator.(&2,&1)))
+    grid_to_key.(end_pos)
+  end
+
+  def generate_sequence(instrs, start, resolver) do 
+   Enum.reduce(instrs, [start], fn(instr, keys) -> ( [ key_after_seq(hd(keys), instr, resolver) ] ++ keys ) end) |> Enum.reverse |> tl
+  end
+
+  def generate_sequence_from_file( filename, start, resolver) do
+    read_input(filename) |> generate_sequence(start, resolver)
   end
   
   def read_input(filename) do
@@ -46,7 +91,9 @@ defmodule Day2 do
   end
 
   def main(_args) do
-    part1 = generate_sequence_from_file("resources/input.txt")
+    part1 = generate_sequence_from_file("resources/input.txt", 5, { &navigate_part1/2, &key_to_grid_part1/1, &grid_to_key_part1/1 })
     IO.inspect part1, label: "Part 1"
+    part2 = generate_sequence_from_file("resources/input.txt", 7, { &navigate_part2/2, &key_to_grid_part2/1, &grid_to_key_part2/1 })
+    IO.inspect part2, label: "Part 2"
   end
 end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -2,23 +2,47 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "navigates to next key" do
-    assert Day2.navigate(5, "U") == [ 1, 0 ]
-    assert Day2.navigate(2, "U") == [ 1, 0 ]
-    assert Day2.navigate(2, "D") == [ 1, 1 ]
-    assert Day2.navigate(7, "D") == [ 0, 2 ]
+  @part1 { &Day2.navigate_part1/2, &Day2.key_to_grid_part1/1, &Day2.grid_to_key_part1/1 }
+  @part2 { &Day2.navigate_part2/2, &Day2.key_to_grid_part2/1, &Day2.grid_to_key_part2/1 }
+
+  test "find correct grid position for part 2" do
+    assert Day2.key_to_grid_part2(7) == [ 0, 0 ]
+  end
+  
+  test "find correct key for grid position for part 2" do
+    assert Day2.grid_to_key_part2([0,0]) == 7
+    assert Day2.grid_to_key_part2([0,2]) == "d"
+    assert Day2.grid_to_key_part2([0,1]) == "b"
+    assert Day2.grid_to_key_part2([-1,1]) == "a"
+  end
+
+  test "navigates to next key for part 2" do
+    assert Day2.navigate_part2(7, "U") == [ 0, -1 ]
+    assert Day2.navigate_part2(2, "U") == [ -1, -1 ]
+    assert Day2.navigate_part2(8, "D") == [ 1, 1 ]
+    assert Day2.navigate_part2(13, "U") == [ 0, 1 ]
+    assert Day2.navigate_part2(13, "L") == [ 0, 2 ]
+    assert Day2.navigate_part2(11, "R") == [ 1, 1 ]
+    assert Day2.navigate_part2(4, "L") == [ 0, -1 ]
+  end
+
+  test "navigates to next key for part 1" do
+    assert Day2.navigate_part1(5, "U") == [ 0, -1 ]
+    assert Day2.navigate_part1(2, "U") == [ 0, -1 ]
+    assert Day2.navigate_part1(2, "D") == [ 0, 0 ]
+    assert Day2.navigate_part1(7, "D") == [ -1, 1 ]
   end
 
   test "get key code" do
-    assert Day2.key_after_seq(5, "ULL") == 1
-    assert Day2.key_after_seq(1, "RRDDD") == 9
-    assert Day2.key_after_seq(9, "LURDL") == 8
-    assert Day2.key_after_seq(8, "UUUUD") == 5
-    assert Day2.key_after_seq(3, "LLLDDUDRLDLRLDUDU") == 4
+    assert Day2.key_after_seq(5, "ULL", @part1) == 1
+    assert Day2.key_after_seq(1, "RRDDD", @part1) == 9
+    assert Day2.key_after_seq(9, "LURDL", @part1) == 8
+    assert Day2.key_after_seq(8, "UUUUD", @part1) == 5
+    assert Day2.key_after_seq(3, "LLLDDUDRLDLRLDUDU", @part1) == 4
   end
 
   test "generates key sequence" do
-    assert Day2.generate_sequence([ "ULL", "RRDDD", "LURDL", "UUUUD" ]) == [ 1, 9 , 8 , 5 ]
+    assert Day2.generate_sequence([ "ULL", "RRDDD", "LURDL", "UUUUD" ], 5, @part1) == [ 1, 9 , 8 , 5 ]
   end
 
   test "reads input" do
@@ -26,6 +50,6 @@ defmodule Day2Test do
   end
 
   test "generate key sequence from file" do
-    assert Day2.generate_sequence_from_file( "resources/input_test.txt" ) == [ 1, 9, 8, 5 ]
+    assert Day2.generate_sequence_from_file( "resources/input_test.txt", 5, @part1 ) == [ 1, 9, 8, 5 ]
   end
 end
